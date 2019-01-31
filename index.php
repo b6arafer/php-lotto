616f2772a1449cc0692d2689f74fc8ea73af7a39
diak <diak@10.9.8.5>
2019-01-31 08:09:36 +0100

    php parancs: echo

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
         LOL
         <?php
         // put your code here
+            //lottohuzas
+            $kihuzottak=0;
+            while($kihuzottak<5)
+            {
+                $sorsoltszam=rand(1,90);
+                if(!isset($tomb[$sorsoltszam]))
+                {
+                $tomb[$sorsoltszam]=1;
+                $kihuzottak++;
+                }
+            }
+            var_dump($tomb);
         echo"PHP";
         ?>
     </body>
