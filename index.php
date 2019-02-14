6a78153cf2ace4470ad76d5e4a47e2d35cfcd91b
diak <diak@10.9.8.5>
2019-02-14 08:01:28 +0100

    fuggveny file

--- index.php
+++ index.php
@@ -7,21 +7,7 @@
     <body>
         LOL
         <?php
-            function sorsolas()
-            {
-                $kihuzottak=0;
-                while($kihuzottak<5)
-                {
-                    $sorsoltszam=rand(1,90);
-                    if(!isset($tomb[$sorsoltszam]))
-                    {
-                    $tomb[$sorsoltszam]=1;
-                    $kihuzottak++;
-                    }
-                }
-                return $tomb;
-                //var_dump($tomb);
-            }
+            include 'fuggveny.php';
         // put your code here
             $sorsolt=sorsolas();
             var_dump($sorsolt);
