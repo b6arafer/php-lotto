38ab316f2152e3065e3a52fa5d8cba24cc7f9f3a
tanar <tanar@10.9.8.100>
2019-02-19 07:49:51 +0100

    megjelenites

--- index.php
+++ index.php
@@ -7,13 +7,37 @@
     <body>
         Ez az index.php
         <?php
-			include("fgv.php");
-			$sorsolt=sorsolas();
-			var_dump($sorsolt);
-			// lottóhúzás
-                        echo "Ez volt a szám tömb";
-            echo "Itt már a php script fut";
-        // put your code here
+            include("fgv.php");
+            $sorsolt=sorsolas();
+            var_dump($sorsolt);
+            
+            $szamokszama=90;
+            $oszlop=15;
+            echo '<table border="1">';
+            for($i=1; $i<=$szamokszama; $i++)
+            {
+                    if($i%$oszlop==1)
+                    {
+                            echo "<tr>";
+                    }
+                    if(isset($sorsolt[$i]))
+                    {
+                            $hatterszin="#FF0000";
+                    }
+                    else
+                    {
+                            $hatterszin="#FFFFFF";
+                    }
+                    echo "<td bgcolor=\"$hatterszin\" >";
+                            echo $i;
+                    echo "</td>";
+                    if($i%$oszlop==0)
+                    {
+                            echo "</tr>\n";
+                    }
+            }
+
+            echo "</table>";
         ?>
     </body>
 </html>
