1db4fcf4d15b9b5225d53f8f0a3037eeb56d2172
tanar <tanar@10.9.8.100>
2019-02-20 09:53:29 +0100

    lottosorsolas osztaly

---
+++ lottosorsolas.php
@@ -0,0 +1,66 @@
+<?php
+
+class lottosorsolas {
+    var $hanyas;
+    var $szamokszama;
+    var $oszlopszam;
+    function __construct($hanyas) 
+    {
+        if($hanyas==5)
+        {
+            $this->hanyas=5;
+            $this->szamokszama=5;
+            $this->oszlop=15;
+        }
+    }
+    
+    
+    
+    function sorsolas()
+    {
+	$kihuzottak=0;
+	while($kihuzottak<$this->hanyas)
+	{
+			$sorsoltszam=rand(1,$this->szamokszama);
+			if(!isset($tomb[$sorsoltszam]))
+			{
+					$tomb[$sorsoltszam]=1;
+					$kihuzottak++;
+			}
+	}
+	return $tomb;
+	// var_dump($tomb);
+    }
+    
+    
+    
+    
+    function lottoszelveny($sorsolt)
+    {
+        echo '<table border="1">';
+        for($i=1; $i<=$this->szamokszama; $i++)
+        {
+                if($i%$this->oszlop==1)
+                {
+                        echo "<tr>";
+                }
+                if(isset($sorsolt[$i]))
+                {
+                        $hatterszin="#FF0000";
+                }
+                else
+                {
+                        $hatterszin="#FFFFFF";
+                }
+                echo "<td bgcolor=\"$hatterszin\" >";
+                        echo $i;
+                echo "</td>";
+                if($i%$this->oszlop==0)
+                {
+                        echo "</tr>\n";
+                }
+        }
+
+        echo "</table>";
+    }
+}
